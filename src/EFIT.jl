037bfa1fb1c13e9424a25e9ead6249e1b9c57dec
Luke Stagner <lstagner@uci.edu>
2018-07-25 02:01:01 -0700

    add utilities for calculating triangularity and ellipticity

--- src/EFIT.jl
+++ src/EFIT.jl
@@ -3,7 +3,9 @@ __precompile__()
 module EFIT
 
 include("io.jl")
-
 export readg, GEQDSKFile
+
+include("utils.jl")
+export triangularity, ellipticity
 
 end # module
---
+++ src/utils.jl
@@ -0,0 +1,19 @@
+function triangularity(g::GEQDSKFile)
+    Rmin, Rmax = extrema(g.rbbbs)
+    Rgeo = (Rmin + Rmax)/2
+    a = (Rmax - Rmin)/2
+    Rupper = g.rbbbs[indmax(g.zbbbs)]
+    Rlower = g.rbbbs[indmin(g.zbbbs)]
+    delta_upper = (Rgeo - Rupper)/a
+    delta_lower = (Rgeo - Rlower)/a
+
+    return delta_lower, delta_upper
+end
+
+function ellipticity(g::GEQDSKFile)
+    Rmin, Rmax = extrema(g.rbbbs)
+    a = (Rmax - Rmin)/2
+    Zmin, Zmax = extrema(g.zbbbs)
+
+    return (Zmax - Zmin)/a
+end
