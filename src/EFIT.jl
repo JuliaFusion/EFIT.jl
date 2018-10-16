f2ec0cb178247725f3b920e0fdec03d9032ed226
Luke Stagner <stagnerl@fusion.gat.com>
2018-10-16 09:13:41 -0700

    add variable pointing to test_gfile

--- src/EFIT.jl
+++ src/EFIT.jl
@@ -8,4 +8,6 @@ export readg, GEQDSKFile
 include("utils.jl")
 export triangularity, ellipticity, elongation, minor_radius, major_radius, aspect_ratio
 
+const test_gfile = (@__DIR__)*"/../test/g000001.01000"
+
 end # module
