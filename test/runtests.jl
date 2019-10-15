ae09d819d202493499cd5f36911caeedf3103d82
Luke Stagner <stagnerl@fusion.gat.com>
2019-10-15 16:32:48 -0700

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
+using Test
 using EFIT
-using Base.Test
 
-# write your own tests here
-@test 1 == 2
+g = readg(EFIT.test_gfile)
+
+lt, ut = triangularity(g)
+@test lt == 0.059614676027860296
+@test ut == 0.05822145848512557
+
+@test ellipticity(g) == 1.475784591289634
+@test major_radius(g) == 1.648852555
+@test minor_radius(g) == 0.633397135
+@test aspect_ratio(g) == 2.6031891587258285
+
