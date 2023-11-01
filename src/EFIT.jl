8c6dee8feea88db1d9432a35fe9f885948b4493b
David Eldon <eldond@fusion.gat.com>
2023-11-01 14:52:57 -0700

    Export new functions

--- src/EFIT.jl
+++ src/EFIT.jl
@@ -7,6 +7,7 @@ export readg, GEQDSKFile
 
 include("utils.jl")
 export triangularity, ellipticity, elongation, minor_radius, major_radius, aspect_ratio, elevation
+export x_points, fluxinfo
 
 const test_gfile = (@__DIR__)*"/../test/g000001.01000"
 const test_gfile2 = (@__DIR__)*"/../test/g184833.03600"
