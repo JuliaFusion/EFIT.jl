dfec26167d708937d231a8bf8ef0f42b031e2229
David Eldon <eldond@fusion.gat.com>
2023-11-01 14:16:48 -0700

    Add function for finding X-points

--- src/EFIT.jl
+++ src/EFIT.jl
@@ -9,5 +9,6 @@ include("utils.jl")
 export triangularity, ellipticity, elongation, minor_radius, major_radius, aspect_ratio, elevation
 
 const test_gfile = (@__DIR__)*"/../test/g000001.01000"
+const test_gfile2 = (@__DIR__)*"/../test/g184833.03600"
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using EFIT
 
 g = readg(EFIT.test_gfile)
+g2 = readg(EFIT.test_gfile2)
 
 lt, ut = triangularity(g)
 @test lt == 0.059614676027860296
@@ -12,3 +13,23 @@ lt, ut = triangularity(g)
 @test minor_radius(g) == 0.633397135
 @test aspect_ratio(g) == 2.6031891587258285
 
+@testset "find xpoint" begin
+    psin_tolerance = 0.002
+    xrs, xzs, xpsins, xseps = EFIT.x_points(g, psin_tolerance=psin_tolerance)
+    @test length(xrs) == 0  # this file is limited, so no x-points should be found
+    @test length(xrs) == length(xzs) == length(xpsins) == length(xseps)
+
+    xrs, xzs, xpsins, xseps = EFIT.x_points(g2, psin_tolerance=psin_tolerance)
+    @test length(xrs) > 0
+    @test length(xrs) == length(xzs) == length(xpsins) == length(xseps)
+    @test xseps[1] == 1
+    @test abs(xpsins[1] - 1.0) < psin_tolerance
+    @test xrs[1] > 0
+    @test (xrs[1] > 1.25) & (xrs[1] < 1.26)
+    @test (xzs[1] > -1.17) & (xzs[1] < -1.15)
+
+    @test xseps[2] == 2
+    @test xpsins[2] > (1 + psin_tolerance)
+    @test (xrs[2] > 1.2) & (xrs[2] < 1.33)
+    @test (xzs[2] > 1.04) & (xzs[2] < 1.17)
+end
