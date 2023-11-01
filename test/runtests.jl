1ea76a3120fe5bdf8807da9d5a901d6f38e0744c
David Eldon <eldond@fusion.gat.com>
2023-11-01 14:42:59 -0700

    Add tests for supporting functions related to x-point finding

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,31 @@ lt, ut = triangularity(g)
 @test aspect_ratio(g) == 2.6031891587258285
 
 @testset "find xpoint" begin
+    # fluxinfo
+    izmin = 5
+    irmin = 4
+    r = collect(LinRange(1.2, 1.4, 9))
+    z = collect(LinRange(-1.45, -1.2, 9))
+    rc = r[irmin]
+    zc = z[izmin]
+    lr = 0.0265
+    lz = 0.0279
+    psi = ((z .- zc) .^ 2 ./ (lz^2))' .- (r .- rc) .^ 2 ./ (lr^2) .+ 34.58
+    br, bz, bpol, d = EFIT.fluxinfo(r, z, psi)
+    @test size(br) == size(psi)
+    minbp, minbploc = findmin(bpol)
+    @test minbploc[1] == irmin
+    @test minbploc[2] == izmin
+    @test all(bpol .>= 0.0)
+
+    # find_local_minimum
+    nbh = 2
+    bplm = EFIT.find_local_minimum(bpol, nbh)
+    @test all(bplm[irmin-nbh:irmin+nbh, izmin-nbh:izmin+nbh] .== minbp)
+    @test all(bplm[irmin+nbh+1:end, :] .!= minbp)
+    @test all(bplm[1:irmin-nbh-1, :] .!= minbp)
+
+    # x_point
     psin_tolerance = 0.002
     xrs, xzs, xpsins, xseps = EFIT.x_points(g, psin_tolerance=psin_tolerance)
     @test length(xrs) == 0  # this file is limited, so no x-points should be found
